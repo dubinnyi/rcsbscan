bcf92cee40aaeea87fe3dee844dab7e12a2d839e
Maxim A. Dubinnyi <dumkas@yandex.ru>
2020-02-04 21:50:14 +0300

    file struct4fit.py; fine print of stats

--- fitscan.py
+++ fitscan.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3 -u
 
 from tools import *
+from struct4fit import Struct4Fit
 import argparse
 import time
 
@@ -29,7 +30,7 @@ def main():
     #ref_struct=get_structure_from_file(args.ref_structure)
 
     s4fit = Struct4Fit(args.ref_structure, args.ref_model, args.ref_chain,
-                       args.ref_residues, args.ref_atoms, args.verbose)
+                       args.ref_residues,  args.ref_atoms, args.verbose)
     if not s4fit:
         quit(-1)
 
@@ -40,8 +41,9 @@ def main():
     print("Start fit scan")
     sup = Superimposer()
     t0 = time.time()
-    struct_count = 0
-    res_count = 0
+    count_files = 0
+    count_struct = 0
+    count_res_tuple = 0
     if args.struct:
         for structf in args.struct:
             try:
@@ -49,6 +51,7 @@ def main():
                 structure = get_structure_from_file(structf, format='auto')
                 if not structure:
                     continue
+                count_files = count_files + 1
                 if args.print_header and args.verbose:
                     for key, value in sorted(structure.header.items()):
                         print("{:20} : {}".format(key, value))
@@ -62,7 +65,7 @@ def main():
                         len_aa = 0 if not chain_res_aa else len(chain_res_aa)
                         len_aa_gaps = 0 if not chain_res_aa else len(Seq_aa)
                         len_water = 0 if not seq_water else len(seq_water)
-                        struct_count = struct_count + 1
+                        count_struct = count_struct + 1
                         if args.verbose:
                             print("> {} model= {:2}, chain= {:1}, {:4} ({:4}) AA, start = {:4}, {:3} WAT".
                                   format(file_id, model.get_id(), chain.get_id(),
@@ -74,7 +77,7 @@ def main():
                             (res_to_fit, atoms_to_fit) = get_res_and_atoms(chain_res_aa, resno, s4fit.ref_res_list_len,
                                                                            s4fit.ref_atom_names_set)
                             if atoms_to_fit and len(atoms_to_fit) == len(s4fit.ref_atoms):
-                                res_count = res_count + 1
+                                count_res_tuple = count_res_tuple + 1
                                 sup.set_atoms(s4fit.ref_atoms, atoms_to_fit)
                                 if sup.rms <= args.max_rms:
                                     (r_start, r_seq) = res_list_to_one_letter_string(res_to_fit)
@@ -87,7 +90,10 @@ def main():
                 continue
 
     print("fitscan finished after {:>4d} sec.".format(int(time.time() - t0)))
-    print("{} tuples of length {} in {} structures superimposed.".format(res_count, s4fit.ref_res_list_len, struct_count))
+    print("Statistics:")
+    print("  {:>7} files".format(count_files))
+    print("  {:>7} structures".format(count_struct))
+    print("  {:>7} {}-length tuples of residues".format(count_res_tuple,s4fit.ref_res_list_len))
 
 
 if __name__ == "__main__":
