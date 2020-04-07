7b25dd93f1296a1c165b4176f561b95927cca519
dubinnyi <OxyLuciFerin2@>
2020-04-07 15:57:04 +0300

    Move *_Hit to separate file

--- scan/struct4fit.py
+++ scan/struct4fit.py
@@ -1,29 +1,8 @@
 #!/usr/bin/python3 -u
 
 from scan.biotools import *
-from collections import namedtuple
+from scan.hit import RMSD_Hit, Water_Hit
 from pdbextract import pdb_extract
-
-
-class RMSD_Hit(namedtuple('RMSD_Hit', ['pdb', 'chain', 'model', 'res_start', 'res_end', 'hit_sequence', 'rmsd'])):
-    def __str__(self):
-        out = "RMSD_HIT: {} model= {:>3}, chain= {:1} hit= {:>4} {} {:<4} rms= {:>6.4f}"
-        out = out.format(self.pdb, self.model, self.chain,
-                         self.res_start, self.hit_sequence, self.res_end, self.rmsd)
-        return out
-
-
-class Water_Hit(namedtuple('Water_Hit', ['water_id', 'water_rms'])):
-    def __init__(self):
-        self.water_id = None
-        self.water_rms = None
-
-    def __str__(self):
-        str = ""
-        if self.water_id and self.water_rms:
-            str = "WAT= {:4} wat_rms= {:>6.4f}"
-            str.format(format(self.water_id, self.water_rms))
-        return str
 
 
 class Struct4Fit:
